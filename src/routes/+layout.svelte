a1e32a8b9c1fa5467ae01efecc86e40edba70b84
Dominic German <dominicgerman@gmail.com>
2023-05-05 16:28:20 -0500

    Update styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <header>
 	<div class="home">
-		<img src="/favicon.png" alt="Constellation logo" />
+		<a href="/">
+			<img src="/favicon.png" alt="Constellation logo" />
+		</a>
 		<a href="/">Constellation Men's Ensemble</a>
 	</div>
 	<nav>
-		<a href="/post">Blog</a>
-		<a href="/about">About</a>
 		<a href="/concerts">Concerts</a>
+		<a href="/about">About</a>
+		<a href="/listen">Listen</a>
+		<a href="/support">Support</a>
+		<button>Give</button>
 	</nav>		
 </header>
 <main>
